7b3f83b46c8ad00d4b4f81882201238bead19194
SemyonSuvorov <125412363+SemyonSuvorov@users.noreply.github.com>
2024-12-22 16:49:27 +0300

    added convex

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { ConvexClientProvider } from "@/components/convex-client-provider";
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -24,10 +26,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <ConvexClientProvider>
+            {children}
+        </ConvexClientProvider>
       </body>
     </html>
   );
