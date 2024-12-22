287c576918353e492911da47d34cda403f4b710b
SemyonSuvorov <125412363+SemyonSuvorov@users.noreply.github.com>
2024-12-22 21:50:48 +0300

    `feat: updated auth and user features with custom password provider and user button`

--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -20,6 +20,9 @@ export const SignUpCard = ({setState} : SignUpCardProps) => {
     const [confirmPassword, setConfirmPassword] = useState("");   
     const [pending, setPending] = useState(false);
     const [error, setError] = useState("");
+    const [name, setName] = useState("");
+
+
     const { signIn } = useAuthActions();
     
     const onPasswordSignUp = (e: React.FormEvent<HTMLFormElement>) => {
@@ -30,7 +33,7 @@ export const SignUpCard = ({setState} : SignUpCardProps) => {
         }
 
         setPending(true);
-        signIn("password", { email, password, flow:"signUp"})
+        signIn("password", { name, email, password, flow:"signUp"})
             .catch(() => {
                 setError("Something went wrong");
             })
@@ -57,6 +60,13 @@ export const SignUpCard = ({setState} : SignUpCardProps) => {
             )}
             <CardContent className="space-y-5 px-0 pb-0">
                 <form onSubmit={onPasswordSignUp} className="space-y-2.5">
+                    <Input 
+                        disabled={pending}
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        placeholder="Full Name"
+                        required
+                    />
                     <Input 
                         disabled={pending}
                         value={email}
