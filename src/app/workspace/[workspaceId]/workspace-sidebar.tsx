f10ec2b466e2ff0c0a5f2014f6492fe5b9dd9e73
SemyonSuvorov <125412363+SemyonSuvorov@users.noreply.github.com>
2024-12-25 19:56:46 +0300

    `feat: add invite and create channel functionality`

--- src/app/workspace/[workspaceId]/workspace-sidebar.tsx
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -8,6 +8,7 @@ import { useGetChannels } from "@/features/channels/api/use-get-channels";
 import { WorkspaceSection } from "./workspace-section";
 import { useGetMembers } from "@/features/members/api/use-get-members";
 import { UserItem } from "./user-item";
+import { useCreateChannelModal } from "@/features/channels/store/use-create-channel-modal";
 
 export const WorkspaceSidebar = () => {
     const workspaceId = useWorkspaceId();
@@ -17,6 +18,8 @@ export const WorkspaceSidebar = () => {
     const { data: channels, isLoading: channelsLoading } = useGetChannels({workspaceId});
     const { data: members, isLoading: membersLoading} = useGetMembers({workspaceId});
 
+    const [_open, setOpen] = useCreateChannelModal();
+    
     if(workspaceLoading || memberLoading){
         return (
             <div className="flex flex-col bg-[#5e2c5f] h-full items-center justify-center">
@@ -52,7 +55,7 @@ export const WorkspaceSidebar = () => {
             <WorkspaceSection
                 label="Channels"
                 hint="New channel"
-                onNew={() => {}}
+                onNew={member.role === "admin" ? () => setOpen(true) : undefined}
             >
                 {channels?.map((item) => (
                     <SidebarItem 
